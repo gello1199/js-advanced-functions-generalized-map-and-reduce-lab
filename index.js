6988302c4cb60e3b607c89d96b26a40aa966af2f
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-09-14 15:20:32 -0400

    commit tree f927b0599560aa2fc01ad891f05c4fc1534cf075

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+
+function map(array, funct) {
+    let newArray = [];
+
+    for (let i = 0; i < array.length; i++){
+        let element = array[i];
+        newArray.push(funct(element))
+    }
+    return newArray;
+}
+
+
+function reduce(array, funct, start) {
+    let arr = (!!start) ? start : array[0]
+    let i = (!!start) ? 0 : 1
+
+    for (; i < array.length; i++) {
+        arr = funct(array[i], arr)
+    }
+    return arr
+}
